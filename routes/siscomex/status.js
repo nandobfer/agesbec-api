7e0cbce7e210e4e1f43f801e89c5b009cc700562
nandobfer <nandobfer@yahoo.com.br>
2022-11-23 15:49:09 -0300

    feat: added status route

---
+++ routes/siscomex/status.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const config = require('../../config.json')
+const newMysql = require('../../src/database')
+
+router.post('/', (request, response, next) => {    
+	const data = request.body;
+
+	const mysql = newMysql(config.database);
+	mysql.connect();
+	
+	mysql.query({
+		sql: `SELECT * FROM activity`,
+		timeout: 40000, // 40s
+	}, (error, results) => {
+		if (error) console.error(error);
+
+        response.json(results)
+        mysql.end()
+	});
+
+
+});
+
+module.exports = router;
