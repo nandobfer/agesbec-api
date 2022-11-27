a851d1e2e3377a156f9c74b153a88cec93f22e4a
nandobfer <nandobfer@yahoo.com.br>
2022-11-27 16:11:29 -0300

    test: resend

--- routes/siscomex/resend_request.js
+++ routes/siscomex/resend_request.js
@@ -1,15 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../../config.json')
+const request = require('request')
+const fs = require('fs')
 
-router.post('/', (request, response, next) => {    
-    const data = request.body
+router.post('/', (_request, response, next) => {    
+    const data = _request.body
     const cert = {crt: config.certificate.dir + config.certificate.crt, pem: config.certificate.dir + config.certificate.pem}
 
     console.log(cert)
     console.log(data)
 
-    response.json({test: cert})
+    const options = {     
+        url: config.url,     
+        headers: {       
+          "User-Agent": "node.js"     
+        },     
+        strictSSL: false,     
+        form: {       
+          // currency, language, provider-specific options here     
+        },     
+        cert: fs.readFileSync(cert.crt),     
+        key: fs.readFileSync(cert.pem),     
+        passphrase: process.env.PASSPHRASE 
+      };  
+        
+        request.post(options, (err, httpResponse, body) => { 
+            console.log(body)
+            response.json({test: body})
+      })
 
 });
 
