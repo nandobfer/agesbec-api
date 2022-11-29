e1f63911105e70d322cda8f29ca4c5b1121caac3
nandobfer <nandobfer@yahoo.com.br>
2022-11-29 10:53:58 -0300

    test: search

--- routes/siscomex/search.js
+++ routes/siscomex/search.js
@@ -8,20 +8,18 @@ router.post('/', (request, response, next) => {
 	const mysql = newMysql(config.database);
 	mysql.connect();
 
-    console.log(data)
-	
-	// mysql.query({
-	// 	sql: `SELECT * FROM acessos WHERE nome LIKE "%?%" order by id DESC`,
-	// 	timeout: 40000,
-    //     values = [
+	mysql.query({
+		sql: `SELECT * FROM acessos WHERE nome LIKE "%?%" order by id DESC`,
+		timeout: 40000,
+        values: [
+            data.name
+        ]
+	}, (error, results) => {
+		if (error) console.error(error);
+        response.json(results)
+        mysql.end()
 
-    //     ]
-	// }, (error, results) => {
-	// 	if (error) console.error(error);
-    //     response.json(results)
-    //     mysql.end()
-
-	// });
+	});
 
 
 });
