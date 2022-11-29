4653e89c485b566b97e34c40f18bb5efa547d224
nandobfer <nandobfer@yahoo.com.br>
2022-11-29 10:57:06 -0300

    test: search

--- routes/siscomex/search.js
+++ routes/siscomex/search.js
@@ -8,17 +8,11 @@ router.post('/', (request, response, next) => {
 	const mysql = newMysql(config.database);
 	mysql.connect();
 
-    console.log(`SELECT * FROM acessos WHERE nome LIKE "%${data.name}%" order by id DESC`)
-
 	mysql.query({
 		sql: `SELECT * FROM acessos WHERE nome LIKE "%${data.name}%" order by id DESC`,
 		timeout: 40000,
-        values: [
-            data.name
-        ]
 	}, (error, results) => {
 		if (error) console.error(error);
-        console.log(results)
         response.json(results)
         mysql.end()
 
